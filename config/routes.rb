abb0409c593053aaf7c4ba4b60ab262c98f27919
amingilani <aminshahgilani@gmail.com>
2016-09-29 00:04:46 +0500

    Generate a home page with a login link

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'public_pages/home'
+
   # Auth0 routes for authentication
   get '/auth/auth0/callback' => 'auth0#callback'
   get '/auth/failure' => 'auth0#failure'
