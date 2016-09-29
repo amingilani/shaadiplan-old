7550a5c5099fe9401e6ff8391bdfbc369a514e98
amingilani <aminshahgilani@gmail.com>
2016-09-30 03:38:54 +0500

    Secrets handled properly

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,9 +1,10 @@
+# Configure the middleware
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :auth0,
-    'TIKTNP8kYglAOMR6jhOEBBEi0qJUxFCI',
-    'Z4bDzH-f0t1VSMCc7NsTE81m2Y_gLzwhsichz90hQqfci_XrojKOMbGwsj_wgtkU',
-    'gilani.auth0.com',
+    Rails.application.secrets.auth0_client_id,
+    Rails.application.secrets.auth0_client_secret,
+    Rails.application.secrets.auth0_domain,
     callback_path: '/auth/auth0/callback'
   )
 end
