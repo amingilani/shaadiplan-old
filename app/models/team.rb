ebe2f4cb57354bd269bddbbe276d93708a69af33
amingilani <aminshahgilani@gmail.com>
2016-09-30 03:50:49 +0500

    Generate teams table

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,30 @@
+# <!-- BEGIN MODEL ANNOTATION -->
+# ## Schema Information
+#
+# Table name: `teams`
+#
+# ### Columns
+#
+# Name              | Type               | Attributes
+# ----------------- | ------------------ | ---------------------------
+# **`id`**          | `integer`          | `not null, primary key`
+# **`color`**       | `integer`          |
+# **`wedding_id`**  | `integer`          |
+# **`created_at`**  | `datetime`         | `not null`
+# **`updated_at`**  | `datetime`         | `not null`
+#
+# ### Indexes
+#
+# * `index_teams_on_wedding_id`:
+#     * **`wedding_id`**
+#
+# ### Foreign Keys
+#
+# * `fk_rails_08b347071b`:
+#     * **`wedding_id => weddings.id`**
+#
+# <!-- END MODEL ANNOTATION -->
+
 class Team < ApplicationRecord
   belongs_to :wedding
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160929224054) do
+ActiveRecord::Schema.define(version: 20160929224950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,10 +32,19 @@ ActiveRecord::Schema.define(version: 20160929224054) do
     t.index ["weddings_id"], name: "index_guests_on_weddings_id", using: :btree
   end
 
+  create_table "teams", force: :cascade do |t|
+    t.integer  "color"
+    t.integer  "wedding_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["wedding_id"], name: "index_teams_on_wedding_id", using: :btree
+  end
+
   create_table "weddings", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   add_foreign_key "guests", "weddings", column: "weddings_id"
+  add_foreign_key "teams", "weddings"
 end
