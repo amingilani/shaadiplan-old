031e169ea64f40472edbd565858b00565be42b3a
amingilani <aminshahgilani@gmail.com>
2016-09-30 03:40:33 +0500

    Update the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,20 @@
+require 'auth0'
+
 class User
   include ActiveModel::Model
-  attr_accessor :name, :age
+
+  def initialize
+    auth0_api.user
+  end
+
+  private
+
+  def auth0_api
+    Auth0Client.new(
+      client_id: Rails.application.secrets.auth0_client_id,
+      token: Rails.application.secrets.auth0_management_jwt,
+      domain: Rails.application.secrets.auth0_domain,
+      api_version: '2'
+    )
+  end
 end
