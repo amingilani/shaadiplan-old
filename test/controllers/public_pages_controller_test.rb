6e12c211bbabdc73f175042119aea09fbc78f9e1
amingilani <aminshahgilani@gmail.com>
2016-10-10 01:38:35 +0500

    Remove the old test directoy

--- test/controllers/public_pages_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PublicPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get public_pages_home_url
-    assert_response :success
-  end
-
-end
