64800a1a80138f59fd73e16c9ca8ff1fe842db5e
amingilani <amin@gilani.me>
2016-10-16 09:24:12 +0500

    Create partials

---
+++ app/helpers/notice_helper.rb
@@ -0,0 +1,16 @@
+module NoticeHelper
+  def message_class_for(flash_type)
+    case flash_type
+    when 'success'
+      'success' # Green
+    when 'error'
+      'error' # Red
+    when 'alert'
+      'warning' # Yellow
+    when 'notice'
+      'info' # Blue
+    else
+      flash_type
+    end
+  end
+end
